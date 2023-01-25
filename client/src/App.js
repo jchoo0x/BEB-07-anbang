b54881bc1fbdfa0cec1d8c6777b72a2be4f61956
jchoo0x <jchoo0x@icloud.com>
2023-01-25 21:11:14 +0900

    Register component

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
-
-import './App.css';
-import React, {useState} from "react";
+import "./App.css";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Register from "./components/Register";
 
 function App() {
   return (
-    <div className='app'>
-        <Header/>
-        {/* <Routes>
+    <div className="app">
+      <Header />
+
+      {/* <Routes>
           <Route path ="/" element={<Main />}/>
           <Route path="/mypage" element={<Mypage account={account}/>}/>
           <Route path="/mint" element={<Mint />}/>
@@ -17,8 +18,7 @@ function App() {
           <Route path="/signup" element={<Signup/>}/>
           <Route path="/login" element={<Login />}/>
         </Routes> */}
-        <Footer/>
-
+      <Footer />
     </div>
   );
 }
