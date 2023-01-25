71163a56c63c51f7074ed18905e749646bc0984a
Beomhyuck <103881061+Beomhyuck@users.noreply.github.com>
2023-01-25 11:35:26 +0900

    개발환경 작성

---
+++ server/server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const cors = require('cors');
+const session = require('express-session');
+
+const apiRouter = require('./router/api');
+
+const http = require('http').createServer(app);
+http.listen(8080, function () {
+  console.log(`listening port 8080`);
+});
+
+
+// 세션을 사용해서 로그인
+app.use(
+  session({
+    secret:'AnBang',
+    resave: false,
+    saveUninitialized: true,
+    // cookie:{
+    //   // domain: 'localhost',
+    //   path:'/',
+    //   maxAge: 24*6*60*10000,
+    //   sameSite: 'NONE',
+    //   httpOnly: false,
+    //   secure: true
+    // }
+  })
+)
+
+app.use(express.json());
+// app.use(cors());
+
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true
+}));
+
+app.use(express.urlencoded({ extended: false }));
+
+// app.use(express.static(path.join(__dirname, `../client/build`)));
+
+app.use('/', apiRouter);
+
+// app.get('/', function (req, res) {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
+
+// app.get('*', function (req, res) {
+//   res.sendFile(path.join(__dirname, `../client/build/index.html`));
+// });
