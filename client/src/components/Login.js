2bc0e6738f601b78b30bdd17335cabfbad74aae9
jchoo0x <jchoo0x@icloud.com>
2023-01-25 14:11:39 +0900

    login

---
+++ client/src/components/Login.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Perform login logic here
+  };
+
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <form onSubmit={handleSubmit}>
+        <label>
+          ID:
+          <input
+            type="text"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </label>
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
