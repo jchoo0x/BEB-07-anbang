31c2f1ed4594a6a7c23b5ae7bb1d5b2831eb3bb8
송해현 <jea752@daum.net>
2023-01-26 16:04:52 +0900

    preview2

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React, {useState} from "react";
-import Header from "./components/header";
-import Footer from "./components/footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Preview from "./pages/preview";
 
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
