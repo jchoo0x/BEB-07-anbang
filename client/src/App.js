9ec5828c63cad3bcba68dcf95860b998e16331f2
송해현 <jea752@daum.net>
2023-01-27 15:13:43 +0900

    NFT

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,10 @@ import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import Main from './pages/Main';
 import Token from './pages/Token';
+import NFTdetail from './pages/NFTdetail';
 
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
+
 
 function App() {
   return (
@@ -20,6 +22,7 @@ function App() {
             <Route path="/login" element={<Login />}/>
             <Route path="/main" element={<Main />}/>
             <Route path="/main" element={<Token />}/>
+            <Route path="/main" element={<NFTdetail />}/>
           </Routes> 
         <Footer/>
 
