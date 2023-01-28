eb7d284037bd229768b9b71f9bbd0fd305238c2d
송해현 <jea752@daum.net>
2023-01-29 02:51:28 +0900

    css

--- client/src/pages/Minting.js
+++ client/src/pages/Minting.js
@@ -1,5 +1,5 @@
 // modules
-import {useState} from "react";
+import React, {useEffect, useState, useRef} from 'react';
 import {Link} from "react-router-dom";
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route, Routes, Switch} from 'react-router-dom';
@@ -10,8 +10,9 @@ import "../assets/css/main.css";
 
 export default function Register() {
 
+    
 
     return(
-        <></>
+        <div className="mt-20 flex flex-col items-center">asd</div>
     )
 }
