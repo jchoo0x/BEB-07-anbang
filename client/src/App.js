9599895fd404bcb4f3fdc35a63a03ac3663c7ae5
jchoo0x <jchoo0x@icloud.com>
2023-01-29 21:53:27 +0900

    RegisterNFT.js 일부 작성

--- client/src/App.js
+++ client/src/App.js
@@ -3,35 +3,32 @@ import React, { useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Preview from "./pages/preview";
-import Login from './pages/Login';
-import SignUp from './pages/SignUp';
-import Main from './pages/Main';
-import Token from './pages/Token';
-import NFTdetail from './pages/NFTdetail';
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import Main from "./pages/Main";
+import Token from "./pages/Token";
+import NFTdetail from "./pages/NFTdetail";
 import Mypage from "./pages/Mypage";
 import Message from "./pages/Message";
-
+import RegisterNFT from "./pages/RegisterNFT";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-
-
 
 function App() {
   return (
-    <div className='app'>
-        <Header/>
-          <Routes>
-            <Route path ="/" element={<Preview/>}/>
-            <Route path="/signup" element={<SignUp/>}/>
-            <Route path="/login" element={<Login />}/>
-            <Route path="/main" element={<Main />}/>
-            <Route path="/token" element={<Token />}/>
-            <Route path="/NFTdetail" element={<NFTdetail />}/>
-            <Route path="/mypage" element={<Mypage />}/>
-            <Route path="/message" element={<Message />}/>
-          </Routes> 
-        <Footer/>
-
+    <div className="app">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Preview />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="/token" element={<Token />} />
+        <Route path="/NFTdetail" element={<NFTdetail />} />
+        <Route path="/mypage" element={<Mypage />} />
+        <Route path="/message" element={<Message />} />
+        <Route path="/register" element={<RegisterNFT />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
--- client/src/pages/RegisterNFT.js
+++ client/src/pages/RegisterNFT.js
@@ -1,17 +1,57 @@
 // modules
-import {useState} from "react";
-import {Link} from "react-router-dom";
-import ReactDOM from 'react-dom'
-import { BrowserRouter, Route, Routes, Switch} from 'react-router-dom';
-
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes, Switch } from "react-router-dom";
 
 // stylesheet
 import "../assets/css/main.css";
 
 export default function Register() {
+  const [image, setImage] = useState(null);
+  const handleImageChange = (event) => {
+    setImage(event.target.files[0]);
+  };
 
+  return (
+    <div class="mb-6 pt-4">
+      <div className="flex ">
+        <div className="mx-auto w-full max-w-lg rounded-lg px-10 py-8 shadow-xl">
+          <div className="mx-auto space-y-6">
+            설명문
+            <div className="flex w-full justify-center"></div>
+          </div>
+        </div>
+      </div>
+      <label class="mb-5 pt-10 px-10 block text-xl font-semibold text-[#07074D]">
+        사진 등록
+      </label>
 
-    return(
-        <></>
-    )
+      <div class="mb-8">
+        <input
+          type="file"
+          name="file"
+          id="file"
+          class="sr-only"
+          onChange={handleImageChange}
+        />
+        <label
+          for="file"
+          class="relative flex min-h-[200px] items-center justify-center rounded-md border border-dashed border-[#e0e0e0] p-12 text-center"
+        >
+          <div>
+            <span class="mb-2 block text-xl font-semibold text-[#07074D]">
+              Drop files here
+            </span>
+            <span class="mb-2 block text-base font-medium text-[#6B7280]">
+              Or
+            </span>
+            <span class="inline-flex rounded border border-[#e0e0e0] py-2 px-7 text-base font-medium text-[#07074D]">
+              Browse
+            </span>
+          </div>
+        </label>
+      </div>
+    </div>
+  );
 }
