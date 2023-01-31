21ab99f04d66b2a373b128ecf94d049b5d196b24
jchoo0x <jchoo0x@icloud.com>
2023-01-31 17:44:38 +0900

    충돌 해결 목적 다시 커밋

--- client/src/pages/Minting.js
+++ client/src/pages/Minting.js
@@ -5,30 +5,34 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes, Switch } from "react-router-dom";
 import Postcode from "../components/Postcode";
 
-
 // stylesheet
 import "../assets/css/main.css";
 
 export default function Register() {
-  
-    const [image, setImage] = useState(null);
+  const [image, setImage] = useState(null);
+  const [imagePreview, setImagePreview] = useState(null);
+  const [file, setFile] = useState(null);
+
   const handleImageChange = (event) => {
     setImage(event.target.files[0]);
-    console.log(image);
+    setImagePreview(URL.createObjectURL(event.target.files[0]));
   };
 
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+    console.log(file);
+  };
 
   return (
     <div className="mb-6 pt-4">
       <div className="flex ">
         <div className="mx-auto w-full max-w-lg rounded-lg px-10 py-8 shadow-xl">
           <div className="mx-auto space-y-6">
-            설명
+            설명문
             <div className="flex w-full justify-center"></div>
           </div>
         </div>
       </div>
-
       <label className="mb-5 pt-10 px-10 block text-xl font-semibold text-[#07074D]">
         사진 등록
       </label>
@@ -66,4 +70,35 @@ export default function Register() {
         </label>
       </div>
       <Postcode />
+      <label class="mb-5 pt-10 px-10 block text-xl font-semibold text-[#07074D]">
+        등기부등본 등록
+      </label>
 
+      <div class="mb-8">
+        <input
+          type="file"
+          name="file"
+          id="file"
+          class="sr-only"
+          onChange={handleFileChange}
+        />
+        <label
+          for="file"
+          class="relative flex min-h-[200px] items-center justify-center rounded-md border border-dashed border-[#e0e0e0] p-12 text-center"
+        >
+          <div>
+            <span class="mb-2 block text-xl font-semibold text-[#07074D]">
+              Drop files here
+            </span>
+            <span class="mb-2 block text-base font-medium text-[#6B7280]">
+              Or
+            </span>
+            <span class="inline-flex rounded border border-[#e0e0e0] py-2 px-7 text-base font-medium text-[#07074D]">
+              Browse
+            </span>
+          </div>
+        </label>
+      </div>
+    </div>
+  );
+}
