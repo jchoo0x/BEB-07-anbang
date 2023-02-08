5958991fc5a4c920d9471a6e138fefa46bb06a8c
hgunh16 <hgunh16@gmail.com>
2023-02-08 16:51:40 +0900

    usemetamask 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import React, { useContext, useState } from "react";
 import axios from "axios";
-import Header from "./components/header";
-import Footer from "./components/footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Preview from "./pages/preview";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
