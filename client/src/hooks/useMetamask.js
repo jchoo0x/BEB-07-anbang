3b33f7397423519b8cfd431a1c078c9c02121ad1
Beomhyuck <103881061+Beomhyuck@users.noreply.github.com>
2023-02-08 18:24:31 +0900

    conflict fix

--- client/src/hooks/useMetamask.js
+++ client/src/hooks/useMetamask.js
@@ -1,82 +1,46 @@
-import { useState, useEffect } from "react";
+// modules
+import {useState, useEffect} from "react";
 
 
-
-const useMetamask = () => {
+// 메타마스크가 브라우저에 설치되어 있는지 확인하고
+// 조건에 모두 해당하면 메타마스크 Provider 객체를 반환합니다.
+const useMetamask = ()=>{
     const [metamask, setMetamask] = useState(null);
 
-    useEffect(() => {
-        (async () => {
-            if (typeof window.ethereum === "undefined") {
+    useEffect(()=>{
+        (async ()=>{
+            if(typeof window.ethereum === "undefined") {
                 alert("지갑이 설치되어있지 않습니다.");
                 return;
             }
 
-            if (window.ethereum.isMetaMask === false) {
+            if(window.ethereum.isMetaMask === false){
                 alert("메타마스크만 지원합니다.");
                 return;
             }
             
-            // Check if the Klaytn network is already added to Metamask
-            const networks = await window.ethereum.request({ method: "eth_chainId" });
-            const KlaytnBaobabChainId = "0x3e9"
-            if(networks===KlaytnBaobabChainId){
-                console.log('correct')
-            }else{
-                console.error('wrong')
-            }
-            // Switch to the Klaytn Baobab network
-            try{
-            await window.ethereum.request({
-                 method: "wallet_switchEthereumChain",
-                 params: [{ chainId: KlaytnBaobabChainId }],
-            });
-            } catch (err){
-                if(err.code = 4902){
-                    console.error('this network is not found in your network')
-                }else{
-                    console.error("failed to switch this network")
-                }
-            }
-            // const isKlaytnAdded = networks.find((network) => network.chainId === "0x39");
-            // If the Klaytn network is not added, add it to Metamask
+            const chaindId = await window.ethereum.request({method:"eth_chainId"});
+            if(chaindId !== "0x5"){
+                const isChange = await window.ethereum
+                .request({method:"wallet_switchEthereumChain", params:[{chainId:"0x5"}]})
+                .catch(err=>err);
 
-            const klaytnBaobabMetaData = {
-                chainId : "0x3e9",
-                chainName : "Klaytn Baobab Network",
-                rpcUrls : ['https://api.baobab.klaytn.net:8651'],
-                blockExplorerUrls : ["https://baobab.scope.klaytn.com"],
-                nativeCurrency : {
-                    name : "Klaytn",
-                    decimals : 18,
-                    symbol : "KLAY" 
-                },
+                if(!isChange) return;
             };
 
-            try{
-             
-                await window.ethereum.request({
-                    method: "wallet_addEthereumChain",
-                    params: [klaytnBaobabMetaData]
-                        // {
-                        //     chainId: "0x39",
-                        //     name: "Klaytn",
-                        //     rpcUrl: "https://baobab.klaytn.net:8651",
-                        // },
-                
-                });
+            window.ethereum.on("chainChanged", async(_chaindId)=>{
+                const isChange = await window.ethereum
+                .request({method:"wallet_switchEthereumChain", params:[{chainId:"0x5"}]})
+                .catch(err=>err);
             
-        }catch(err){
-            console.error(err.message)
-        }
+                if(!isChange) metamask = null;
+              })
 
-
-            // Set the Metamask object
             setMetamask(window.ethereum);
-        })();
-    }, []);
+        })()
+    },[])
 
     return metamask;
-};
+}
 
 export default useMetamask;
