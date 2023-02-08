33e9beb0021e081972a09fe99f26ef5dfdef70f7
hgunh16 <hgunh16@gmail.com>
2023-02-08 18:27:29 +0900

    refresh

--- server/controllers/estate.controller.js
+++ server/controllers/estate.controller.js
@@ -29,13 +29,14 @@ module.exports = {
     register :  async(req,res,next)=>{
         //로그인 검증
         const authorization = req.headers['authorization'];
-        if (!authorization) {
-            return res.status(400).json({ data: null, message: 'invalid access token' });
-        }
+        // if (!authorization) {
+        //     return res.status(400).json({ data: null, message: 'invalid access token' });
+        // }
         try{
-            const token = authorization.split(' ')[1];
-            const data =jwt.verify(token,process.env.ACCESS_SECRET);
-            if(data){
+            // const token = authorization.split(' ')[1];
+            // const data =jwt.verify(token,process.env.ACCESS_SECRET);
+            // if(data)
+            // {
                 const {types, deposit, rental, description, tokenId} = req.body;
             
                 if (!types || !deposit || !description) {
@@ -49,15 +50,16 @@ module.exports = {
                     description,
                     isSelling : true,
                     tokenId,
-                    owner : data.id
+                    // owner : data.id
                 })
             return res.status(200).json(newEstate);
-            }
+            // }
         }catch(err){
             console.error(err.message);
             next(err);
           }
     },
+    
     report: async(req,res,next)=>{
         const authorization = req.headers['authorization'];
         if (!authorization) {
@@ -77,7 +79,7 @@ module.exports = {
                 const newReport = await Report.create({
                     reason,
                     reportId,
-                    reporterId
+                    reporterId : data.id
                 }) 
             
             return res.status(200).json({data : newReport, messgae: 'report success'});
