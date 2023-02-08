a170a385a2377527e5819bcdc83207f0c4dcbfcb
송해현 <jea752@daum.net>
2023-02-08 16:22:51 +0900

    metamask v1

--- client/src/hooks/useMetamask.js
+++ client/src/hooks/useMetamask.js
@@ -15,28 +15,39 @@ const useMetamask = () => {
                 return;
             }
             
-            // Check if the Klaytn network is already added to Metamask
-            const networks = await window.ethereum.request({ method: "wallet_getEthereumNetworks" });
-            const isKlaytnAdded = networks.find((network) => network.chainId === "0x39");
+            
+            const networks = await window.ethereum.request({ method: "eth_chainId" });
+            const KlaytnBaobabChainId = "0x3e9"
+            
+         
 
-            // If the Klaytn network is not added, add it to Metamask
-            if (!isKlaytnAdded) {
+            const klaytnBaobabMetaData = {
+                chainId : "0x3e9",
+                chainName : "Klaytn Baobab Network",
+                rpcUrl : ['https://api.baobab.klaytn.net:8651/'],
+                blockExplorerUrls : ["https://baobab.scope.klaytn.com"],
+                nativeCurrency : {
+                    name : "KLAYTN",
+                    decimals : 18,
+                    symbol : "KLAYTN" 
+                }
+            }
+            try{
+            if (networks !== KlaytnBaobabChainId) {
                 await window.ethereum.request({
                     method: "wallet_addEthereumChain",
-                    params: [
-                        {
-                            chainId: "0x39",
-                            name: "Klaytn",
-                            rpcUrl: "https://baobab.klaytn.net:8651",
-                        },
-                    ],
+                    params: [klaytnBaobabMetaData]
+         
+                
                 });
-            }
-
+            } 
+        }catch(err){
+            console.error(err.message)
+        }
             // Switch to the Klaytn Baobab network
             await window.ethereum.request({
                 method: "wallet_switchEthereumChain",
-                params: [{ chainId: "0x39" }],
+                params: [{ chainId: KlaytnBaobabChainId }],
             });
 
             // Set the Metamask object
