bd14c9e191a953c55a53483327cd2681ff546c10
송해현 <jea752@daum.net>
2023-02-10 14:41:20 +0900

    contract API

--- client/src/pages/ContractAgree.js
+++ client/src/pages/ContractAgree.js
@@ -1,6 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
 function ContractAgree() {
+
+  const [contractInfo, setContractInfo] = useState([]);
+  const [NFTInfo, setNFTInfo] = useState([])
+
+  // 매물정보 가져오기 
+  function getInfo(e){
+    e.preventDefault();
+    axios
+    .get("http://localhost:8080/estate/:id", NFTInfo)
+    .then((res)=>{
+      console.log(res);
+      setNFTInfo([...res.data]);
+    })
+    .catch((e) => console.log(e))
+  }
+
+    // 계약서 get
+    function getContract(e) {
+      e.preventDefault();
+      axios
+        .post("http://localhost:8080/contract/tenantcheck", contractInfo)
+        .then((result) => {
+          console.log(result);
+          setContractInfo([result.data]);
+        })
+        .catch((e) => console.log(e));
+    }
+  
+
   const currentTime = new Date();
   const TwoyearTime = new Date(
     currentTime.setFullYear(currentTime.getFullYear() + 2)
@@ -69,8 +99,9 @@ function ContractAgree() {
           </div>
         </div>
       </div>
-
+      {contractInfo && contractInfo.map((post)=> (
       <div className="flex-auto justify-center w-full mx-auto">
+          {NFTInfo && NFTInfo.map((e)=> (
         <div className="bg-white p-10 flex flex-col w-full shadow-xl rounded-xl">
           <div className="flex items-center justify-center"></div>
           <form action="" class="w-full">
@@ -84,11 +115,13 @@ function ContractAgree() {
                 className="text-black border border-blue-700 bg-white max-w-sm font-mono text-sm py-3 px-4 w-[500px] rounded-md"
               />
             </div>
-            <div id="input" class="flex flex-col justify-center items-center w-full my-5">
+            <div id="input" class="flex flex-col justify-center items-center w-full my-5">              
               <label for="username" class="text-black mb-2">
-                임대주택 유형
+                임대주택 유형 : {e.types}
               </label>
               <input
+                value={e.types}
+                onChange={getInfo}
                 type="text"
                 id="username"
                 className="text-black border border-blue-700 bg-white max-w-sm font-mono text-sm py-3 px-4 w-[500px] rounded-md"
@@ -96,9 +129,11 @@ function ContractAgree() {
             </div>
             <div id="input" class="flex flex-col justify-center items-center w-full my-5">
               <label for="username" class="text-black mb-2">
-                계약기간
+                계약기간 : {post.contractPeriod}
               </label>
               <input
+                value={contractInfo.contractPeriod}
+                onChange={getContract}
                 type="text"
                 id="username"
                 className="text-black border border-blue-700 bg-white max-w-sm font-mono text-sm py-3 px-4 w-[500px] rounded-md"
@@ -106,9 +141,23 @@ function ContractAgree() {
             </div>
             <div id="input" class="flex flex-col justify-center items-center w-full my-5">
               <label for="username" class="text-black mb-2">
-                임대료
+                보증금 : {e.deposit}
               </label>
               <input
+                value={e.deposit}
+
+                type="text"
+                id="username"
+                className="text-black border border-blue-700 bg-white max-w-sm font-mono text-sm py-3 px-4 w-[500px] rounded-md"
+              />
+            </div>
+            <div id="input" class="flex flex-col justify-center items-center w-full my-5">
+              <label for="username" class="text-black mb-2">
+                월세 : {e.rental}
+              </label>
+              <input
+                value={e.rental}
+                onChange={getInfo}
                 type="text"
                 id="username"
                 className="text-black border border-blue-700 bg-white max-w-sm font-mono text-sm py-3 px-4 w-[500px] rounded-md"
@@ -117,19 +166,11 @@ function ContractAgree() {
 
             <div id="input" class="flex flex-col justify-center items-center w-full my-5">
               <label for="username" class="text-black mb-2">
-                임대인 특약조항
+                특약조항 : {post.tenantAgree}
               </label>
               <input
-                type="text"
-                id="username"
-                className="text-black border border-blue-700 bg-white max-w-sm font-mono text-sm py-3 px-4 w-[500px] rounded-md"
-              />
-            </div>
-            <div id="input" class="flex flex-col justify-center items-center w-full my-5">
-              <label for="username" class="text-black mb-2">
-                임차인 특약조항
-              </label>
-              <input
+                value={post.tenantAgree}
+                onChange={getContract}
                 type="text"
                 id="username"
                 className="text-black border border-blue-700 bg-white max-w-sm font-mono text-sm py-3 px-4 w-[500px] rounded-md"
@@ -143,8 +184,10 @@ function ContractAgree() {
             </div>
           </form>
         </div>
+      ))}
       </div>
-
+      
+    ))}
       <form>
         <button
           type="submit"
